1f540a818354f484fde5eef85fa5b68b3a2d2ad3
Maxime MEDAL <medal.max@gmail.com>
2024-01-03 09:19:09 +0100

    fix: delete nameSpace

--- FonctionControlFlow/TestEuclideXUnit/EuclideTests.cs
+++ FonctionControlFlow/TestEuclideXUnit/EuclideTests.cs
@@ -1,25 +1,22 @@
-namespace TestEuclideXUnit
+public class EuclideTests
 {
-    public class EuclideTests
+    [Fact]
+    public void GcdTest_WhenSameValue()
     {
-        [Fact]
-        public void GcdTest_WhenSameValue()
-        {
-            UInt64 n = 12;
-            UInt64 g = Euclide.Gcd(n, n);
-            Assert.Equal(n, g);
-        }
+        UInt64 n = 12;
+        UInt64 g = Euclide.Gcd(n, n);
+        Assert.Equal(n, g);
+    }
 
-        [Theory]
-        [InlineData(123UL, 1921UL)]
-        [InlineData(17UL, 1UL)]
-        [InlineData(1UL, 17UL)]
-        [InlineData(3UL, 5UL)]
+    [Theory]
+    [InlineData(123UL, 1921UL)]
+    [InlineData(17UL, 1UL)]
+    [InlineData(1UL, 17UL)]
+    [InlineData(3UL, 5UL)]
 
-        public void GcdTest_When1(UInt64 a, UInt64 b)
-        {
-            UInt64 g = Euclide.Gcd(a, b);
-            Assert.Equal(1UL, g);
-        }
+    public void GcdTest_When1(UInt64 a, UInt64 b)
+    {
+        UInt64 g = Euclide.Gcd(a, b);
+        Assert.Equal(1UL, g);
     }
 }
